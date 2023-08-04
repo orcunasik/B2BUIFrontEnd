d74be50c9a7ab84f57baf87d581606038f3b46cf
Orçun Aşık <asikorcun@gmail.com>
2023-08-04 17:38:41 +0300

    Added login() method

--- src/app/ui/login/service/auth.service.ts
+++ src/app/ui/login/service/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { LoginModel } from '../model/login-model';
+import { Router } from '@angular/router';
+import { ErrorService } from 'src/app/services/error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,9 @@ export class AuthService {
   constructor(
     @Inject("apiUrl") 
     private apiUrl : string,
-    private httpClient : HttpClient
+    private httpClient : HttpClient,
+    private router :Router,
+    private errorService : ErrorService
   ) { }
 
   isAuth(): boolean{
@@ -17,5 +22,18 @@ export class AuthService {
       return true;
     }
     return false;
+  }
+  login(loginForm : any){
+    let api = this.apiUrl + "Auth/CustomerLogin";
+    let model : LoginModel = new LoginModel();
+    model.email = loginForm.value.email;
+    model.password = loginForm.value.password;
+
+    this.httpClient.post(api,model).subscribe((res:any)=>{
+      localStorage.setItem("token",res.data.customerAccessToken)
+      this.router.navigate(["/"]);
+    },(err)=>{
+      this.errorService.errorHandler(err);
+    });
   }
 }
