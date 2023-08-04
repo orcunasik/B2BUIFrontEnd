5740a3a8fbc4afcc8235f9511e36855a4e2c4664
Orçun Aşık <asikorcun@gmail.com>
2023-08-04 16:30:39 +0300

    Added autService injection

--- src/app/ui/home/detail/detail.component.ts
+++ src/app/ui/home/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../login/service/auth.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  isAuth : boolean =false;
+  constructor(
+    private authService : AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.isAuth = this.authService.isAuth();
   }
 
 }
--- src/app/ui/layouts/header/header.component.ts
+++ src/app/ui/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../login/service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAuth : boolean = false;
+  constructor(
+    private authService : AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.isAuth = this.authService.isAuth();
   }
 
   logout(){
