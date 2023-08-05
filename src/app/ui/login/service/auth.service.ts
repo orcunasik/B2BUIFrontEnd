e24214d772f04c1a4696a4e42984327c85e16986
Orçun Aşık <asikorcun@gmail.com>
2023-08-05 12:38:54 +0300

    Fixed was login method. Added was logout method.

--- src/app/ui/login/service/auth.service.ts
+++ src/app/ui/login/service/auth.service.ts
@@ -3,6 +3,8 @@ import { Inject, Injectable } from '@angular/core';
 import { LoginModel } from '../model/login-model';
 import { Router } from '@angular/router';
 import { ErrorService } from 'src/app/services/error.service';
+import { catchError, tap, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,11 @@ import { ErrorService } from 'src/app/services/error.service';
 export class AuthService {
 
   constructor(
-    @Inject("apiUrl") 
-    private apiUrl : string,
+    @Inject("apiUrl") private apiUrl : string,
     private httpClient : HttpClient,
     private router :Router,
-    private errorService : ErrorService
+    private errorService : ErrorService,
+    private toastr : ToastrService,
   ) { }
 
   isAuth(): boolean{
@@ -24,16 +26,26 @@ export class AuthService {
     return false;
   }
   login(loginForm : any){
-    let api = this.apiUrl + "Auth/CustomerLogin";
+    const api = this.apiUrl + "Auth/CustomerLogin";
     let model : LoginModel = new LoginModel();
     model.email = loginForm.value.email;
     model.password = loginForm.value.password;
 
-    this.httpClient.post(api,model).subscribe((res:any)=>{
-      localStorage.setItem("token",res.data.customerAccessToken)
-      this.router.navigate(["/"]);
-    },(err)=>{
-      this.errorService.errorHandler(err);
-    });
+    this.httpClient.post(api, model).pipe(
+      tap((res: any) => {
+        localStorage.setItem("token", res.data.customerAccessToken);
+        this.router.navigate(["/"]);
+        this.toastr.success("Giriş Başarılı");
+      }),
+      catchError((err) => {
+        this.errorService.errorHandler(err);
+        return throwError(() => err);
+      })
+    ).subscribe();
+  }
+  logout(){
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
+    this.toastr.info("Çıkış Başarılı");
   }
 }
