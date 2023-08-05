eb94e2d14e68c19b5951b5e3025fa42276ea1917
Orçun Aşık <asikorcun@gmail.com>
2023-08-05 12:41:12 +0300

    Added was logout method and ngAfterContentChecked method.

--- src/app/ui/layouts/header/header.component.ts
+++ src/app/ui/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../login/service/auth.service';
 
 @Component({
@@ -6,19 +6,23 @@ import { AuthService } from '../../login/service/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterContentChecked {
 
   isAuth : boolean = false;
   constructor(
     private authService : AuthService
   ) { }
 
-  ngOnInit(): void {
+  ngAfterContentChecked(): void {
     this.isAuth = this.authService.isAuth();
   }
 
-  logout(){
+  ngOnInit(): void {
     
+  }
+
+  logout(){
+    this.authService.logout();
   }
 
 }
