45dec2fd5d6a84b2f5bdbc5171f630dc0fbee2b0
Orçun Aşık <asikorcun@gmail.com>
2023-08-07 16:04:39 +0300

    added was getCustomerId and getList methods with product,error and customerDecode services.

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './service/product.service';
+import { ProductModel } from './model/product-model';
+import { ErrorService } from 'src/app/services/error.service';
+import { CustomerDecodeService } from '../login/service/customer-decode.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products : ProductModel[] = [];
+  customerId : number = 0;
+  filterText : string ="";
+  constructor(
+    private productService : ProductService,
+    private errorService : ErrorService,
+    private customerDecodeService : CustomerDecodeService
+  ) { }
 
   ngOnInit(): void {
+    this.getCustomerId();
+    this.getList();
+  }
+
+  getCustomerId(){
+    this.customerId = this.customerDecodeService.getCustomerId();
+  }
+
+  getList(){
+    this.productService.getList(this.customerId)
+    .subscribe({
+      next: (res:any) => this.products = res.data,
+      error: (err:any) => this.errorService.errorHandler(err)
+
+    });
   }
 
 }
