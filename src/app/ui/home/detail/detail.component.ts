723b6bd1c664fcfdd08bb4d3cf5a9d1b2d96c941
Orçun Aşık <asikorcun@gmail.com>
2023-08-07 16:10:37 +0300

    edited was ngOnit method for productDetail

--- src/app/ui/home/detail/detail.component.ts
+++ src/app/ui/home/detail/detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../login/service/auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductDetailService } from './service/product-detail.service';
+import { ProductDetailModel } from './model/product-detail-model';
+import { ErrorService } from 'src/app/services/error.service';
 
 @Component({
   selector: 'app-detail',
@@ -8,12 +12,29 @@ import { AuthService } from '../../login/service/auth.service';
 })
 export class DetailComponent implements OnInit {
 
+  productDetails : ProductDetailModel[] = [];
   isAuth : boolean =false;
+  productId : number = 0;
+
   constructor(
-    private authService : AuthService
+    private authService : AuthService,
+    private activatedRoute : ActivatedRoute,
+    private productDetailService : ProductDetailService,
+    private errorService : ErrorService
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .subscribe({
+      next:(res:any) => {
+        this.productId = res.id;
+        this.productDetailService.getList(this.productId)
+        .subscribe({
+          next: (res:any) => this.productDetails = res.data,
+          error : (err:any) => this.errorService.errorHandler(err)
+        });
+      }
+    })
     this.isAuth = this.authService.isAuth();
   }
 
