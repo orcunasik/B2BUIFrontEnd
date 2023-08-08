1656cec13c0704565ebf04734af6044b8afa98a2
Orçun Aşık <asikorcun@gmail.com>
2023-08-08 11:35:54 +0300

    Added orderModel

---
+++ src/app/ui/order/model/order-model.ts
@@ -0,0 +1,10 @@
+export class OrderModel{
+    id : number;
+    customerId : number;
+    customerName : string;
+    orderNumber : string;
+    status : string;
+    date : string;
+    quantity : number;
+    total : number;
+}
