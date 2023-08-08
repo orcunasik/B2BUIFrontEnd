845176c9db25c7d71bfff1e3bcf636e8b46851cb
Orçun Aşık <asikorcun@gmail.com>
2023-08-08 11:19:09 +0300

    Basket operations related crud methods have been added.

--- src/app/ui/basket/basket.component.ts
+++ src/app/ui/basket/basket.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './service/basket.service';
+import { ErrorService } from 'src/app/services/error.service';
+import { ToastrService } from 'ngx-toastr';
+import { CustomerDecodeService } from '../login/service/customer-decode.service';
+import { BasketModel } from './model/basket-model';
+import { OrderService } from '../order/service/order.service';
 
 @Component({
   selector: 'app-basket',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasketComponent implements OnInit {
 
-  constructor() { }
+  baskets : BasketModel[] = [];
+  customerId : number = 0;
+  grandTotal : number = 0;
+
+  constructor(
+    private basketService : BasketService,
+    private errorService : ErrorService,
+    private toastr : ToastrService,
+    private customerDecodeService : CustomerDecodeService,
+    private orderService : OrderService
+  ) { }
 
   ngOnInit(): void {
+    this.getCustomerId();
   }
 
+  getCustomerId(){
+    this.customerId = this.customerDecodeService.getCustomerId();
+    this.getList();
+  }
+
+  getList(){
+    this.basketService.getList(this.customerId)
+                              .subscribe({
+                                next : (res:any) => {
+                                  this.baskets = res.data;
+                                  this.setTotal();
+                                },
+                                error : (err:any) => this.errorService.errorHandler(err)
+                              });
+  }
+
+  setTotal(){
+    this.grandTotal = 0;
+    this.baskets.forEach(p => {
+      this.grandTotal = this.grandTotal + p.total;
+    });
+  }
+
+  createOrder(){
+    this.orderService.add(this.customerId)
+                        .subscribe({
+                          next : (res : any) => {
+                            this.toastr.success("Sipariş Başarıyla Oluşturuldu");
+                            this.getList();
+                          },
+                          error : (err : any) => this.errorService.errorHandler(err)
+                        });
+  }
+
+  delete(basket : BasketModel){
+    this.basketService.delete(basket)
+                        .subscribe({
+                          next : (res : any) => {
+                            this.toastr.warning(basket.productName + " Sepetten Silindi!");
+                            this.getList();
+                          },
+                          error : (err : any) => this.errorService.errorHandler(err)
+                        });
+  }
 }
