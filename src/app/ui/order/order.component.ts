95ad8c0e86998cc2de88c64d79f18495f3c9d8d8
Orçun Aşık <asikorcun@gmail.com>
2023-08-08 11:40:20 +0300

    Edited orderComponent related to order Crud operations.

--- src/app/ui/order/order.component.ts
+++ src/app/ui/order/order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from './service/order.service';
+import { CustomerDecodeService } from '../login/service/customer-decode.service';
+import { ErrorService } from 'src/app/services/error.service';
+import { OrderModel } from './model/order-model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  orders : OrderModel[] = [];
+  customerId : number = 0;
+  filterText : string ="";
+
+  constructor(
+    private orderService : OrderService,
+    private customerDecodeService : CustomerDecodeService,
+    private errorService : ErrorService,
+    private toastr : ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.getCustomerId();
+  }
+
+  getCustomerId(){
+    this.customerId = this.customerDecodeService.getCustomerId();
+    this.getList();
+  }
+
+  getList(){
+    this.orderService.getListByCustomerId(this.customerId)
+      .subscribe({
+            next : (res: any) => this.orders = res.data,
+            error : (err : any) => this.errorService.errorHandler(err)                                
+      })
+  }
+
+  delete(order : OrderModel){
+    this.orderService.delete(order)
+                        .subscribe({
+                          next : (res : any) => {
+                            this.toastr.warning(order.orderNumber + " Nolu Siparişiniz İptal Edildi!");
+                            this.getList();
+                          },
+                          error : (err : any) => this.errorService.errorHandler(err)
+                        });
   }
 
 }
